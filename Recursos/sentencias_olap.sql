5bb825d662f150d7659f3c083d3560940c852753
gnbarzol <gnbarzol@espol.edu.ec>
2022-01-30 20:26:27 -0500

    Sentencias para reporte

---
+++ Recursos/sentencias_olap.sql
@@ -0,0 +1,23 @@
+-- roll-up
+select ds.salesterritorycountry, ds.salesterritoryregion, sum(f.orderquantity) total_quantity
+from factinternetsales f
+join dimsalesterritory ds ON ds.salesterritorykey = f.salesterritorykey
+group by rollup(ds.salesterritorycountry, ds.salesterritoryregion);
+
+
+-- drill-dowwn
+select  d.calendaryear, c.gender, sum(f.orderquantity) total_quantity
+from factinternetsales f
+join dimdate d on f.orderdatekey = d.datekey
+join dimcustomer c on f.customerkey = c.customerkey
+group by (d.calendaryear,c.gender)
+order by d.calendaryear desc
+
+
+-- dice
+select  p.color ,sum(orderquantity) total
+from factinternetsales f
+join dimproduct p on f.productkey = p.productkey
+join dimproductcategory pc on p.productcategorykey = pc.productcategorykey
+where pc.englishproductcategoryname='Bikes'
+group by (p.color)
